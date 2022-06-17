ca41b61eff7849f609766ada7d1b839b1ee78b10
mbenaissa <mehdibenaissa@outook.com>
2022-06-17 15:22:00 +0200

    bdd_dump.sql

--- agent_deploie/bdd_spotifree.sql
+++ agent_deploie/bdd_spotifree.sql
@@ -19,6 +19,8 @@
 -- Table structure for table `musique`
 --
 
+CREATE database spotifree;
+USE spotifree;
 DROP TABLE IF EXISTS `musique`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
